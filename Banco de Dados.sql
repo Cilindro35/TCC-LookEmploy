0c1ff0100b516690fb8c7061ca49145135056fd5
Brenado <bernardovitoriolemenicolas01@gmail.com>
2025-11-20 15:15:40 -0300

    Refactor SQL schema for Cliente, Prestador, Servico, and AnuncioServico

--- Banco de Dados.sql
+++ Banco de Dados.sql
@@ -1,83 +1,83 @@
 CREATE DATABASE LookEmploy;
 USE LookEmploy;
 
-	CREATE TABLE Cliente (
-		
-		ID INT AUTO_INCREMENT PRIMARY KEY,
-		
-		nome VARCHAR(400) NOT NULL,
-		sobrenome VARCHAR(400) NOT NULL,
-		email VARCHAR(400) NOT NULL UNIQUE,
-		senha VARCHAR(400) NOT NULL,
-		telefone VARCHAR(11),
-		bairro VARCHAR(200) NOT NULL,
-		logradouro VARCHAR(200) NOT NULL,
-		numero VARCHAR(10) NOT NULL,
-		complemento VARCHAR(150),
-		sexo VARCHAR(9) NOT NULL,
-		-- genero VARCHAR(40) NOT NULL,
-		dataNascimento DATE,
+CREATE TABLE Cliente (
+	
+	ID INT AUTO_INCREMENT PRIMARY KEY,
+	
+	nome VARCHAR(400) NOT NULL,
+	sobrenome VARCHAR(400) NOT NULL,
+	email VARCHAR(400) NOT NULL UNIQUE,
+	senha VARCHAR(400) NOT NULL,
+	telefone VARCHAR(11),
+	bairro VARCHAR(200) NOT NULL,
+	logradouro VARCHAR(200) NOT NULL,
+	numero VARCHAR(10) NOT NULL,
+	complemento VARCHAR(150),
+	sexo VARCHAR(9) NOT NULL,
+	-- genero VARCHAR(40) NOT NULL,
+	dataNascimento DATE,
 
-		descricao TEXT,
-		caminhoImagemPerfil VARCHAR(255),
-		caminhoImagemFundo VARCHAR(255)		
-		)ENGINE = InnoDB;
+	descricao TEXT,
+	caminhoImagemPerfil VARCHAR(255),
+	caminhoImagemFundo VARCHAR(255)		
+	)ENGINE = InnoDB;
 
-	CREATE TABLE Prestador (
+CREATE TABLE Prestador (
 
-		ID INT AUTO_INCREMENT PRIMARY KEY,
+	ID INT AUTO_INCREMENT PRIMARY KEY,
 
-		nome VARCHAR(400) NOT NULL,
-		sobrenome VARCHAR(400) NOT NULL,
-		email VARCHAR(400) NOT NULL UNIQUE,
-		senha VARCHAR(400) NOT NULL,
-		telefone VARCHAR(11),
-		bairro VARCHAR(200) NOT NULL,
-		logradouro VARCHAR(200) NOT NULL,
-		numero VARCHAR(10) NOT NULL,
-		complemento VARCHAR(150),
-		sexo VARCHAR(9) NOT NULL,
-		-- genero VARCHAR(40) NOT NULL,
-		dataNascimento DATE,
+	nome VARCHAR(400) NOT NULL,
+	sobrenome VARCHAR(400) NOT NULL,
+	email VARCHAR(400) NOT NULL UNIQUE,
+	senha VARCHAR(400) NOT NULL,
+	telefone VARCHAR(11),
+	bairro VARCHAR(200) NOT NULL,
+	logradouro VARCHAR(200) NOT NULL,
+	numero VARCHAR(10) NOT NULL,
+	complemento VARCHAR(150),
+	sexo VARCHAR(9) NOT NULL,
+	-- genero VARCHAR(40) NOT NULL,
+	dataNascimento DATE,
 
-		descricao TEXT,
-		caminhoImagemPerfil VARCHAR(255),
-		caminhoImagemFundo VARCHAR(255),
+	descricao TEXT,
+	caminhoImagemPerfil VARCHAR(255),
+	caminhoImagemFundo VARCHAR(255),
 
-		tipoServico VARCHAR(11),
-		estrelaAvaliativa VARCHAR(5)		
-		)ENGINE = InnoDB;
+	tipoServico VARCHAR(11),
+	avaliacao INT		
+	)ENGINE = InnoDB;
 
-	CREATE TABLE Servico (
+CREATE TABLE Servico (
 
-		codigoServico INT PRIMARY KEY AUTO_INCREMENT,
+	codigoServico INT PRIMARY KEY AUTO_INCREMENT,
 
-		bairro VARCHAR(200) NOT NULL,
-		logradouro VARCHAR(200) NOT NULL,
-		numero VARCHAR(10) NOT NULL,
-		complemento VARCHAR(150),
-		dataServico DATETIME NOT NULL,
-		tipoPagamento VARCHAR(150) NOT NULL,
-		descricao TEXT,
-		contrato TEXT,
+	bairro VARCHAR(200) NOT NULL,
+	logradouro VARCHAR(200) NOT NULL,
+	numero VARCHAR(10) NOT NULL,
+	complemento VARCHAR(150),
+	dataServico DATETIME NOT NULL,
+	tipoPagamento VARCHAR(150) NOT NULL,
+	descricao TEXT,
+	contrato TEXT,
 
-		prestador INT NOT NULL,
-		cliente INT NOT NULL,
+	prestador INT NOT NULL,
+	cliente INT NOT NULL,
 
-		FOREIGN KEY (prestador) REFERENCES Prestador(ID),
-		FOREIGN KEY (cliente) REFERENCES Cliente(ID) 
-		)ENGINE = InnoDB;
+	FOREIGN KEY (prestador) REFERENCES Prestador(ID),
+	FOREIGN KEY (cliente) REFERENCES Cliente(ID) 
+	)ENGINE = InnoDB;
 
-	 CREATE TABLE AnuncioServico (
+	CREATE TABLE AnuncioServico (
 
-		codigoAnuncio INT PRIMARY KEY AUTO_INCREMENT,
+	codigoAnuncio INT PRIMARY KEY AUTO_INCREMENT,
 
-		bairro VARCHAR(200) NOT NULL,
-		logradouro VARCHAR(200) NOT NULL,
-		data DATETIME NOT NULL,
-		descricao TEXT,
-		tipoServico VARCHAR(11),
-		cliente INT NOT NULL,
+	bairro VARCHAR(200) NOT NULL,
+	logradouro VARCHAR(200) NOT NULL,
+	data DATETIME NOT NULL,
+	descricao TEXT,
+	tipoServico VARCHAR(11),
+	cliente INT NOT NULL,
 
-		FOREIGN KEY (cliente) REFERENCES Cliente(ID) 
-		)ENGINE = InnoDB;
+	FOREIGN KEY (cliente) REFERENCES Cliente(ID) 
+	)ENGINE = InnoDB;
